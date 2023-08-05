0e5852488da5a73cb3ec02f141bf88538ee48b37
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:37:53 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -1,8 +1,41 @@
 package controller;
 
+import java.util.Scanner;
+
 public class CalculateBMI {
 
-	
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your weight in kilograms: ");
+        double weight = scanner.nextDouble();
+
+        System.out.print("Enter your height in meters: ");
+        double height = scanner.nextDouble();
+
+        double bmi = calculateBMI(weight, height);
+        System.out.println("Your BMI is: " + bmi);
+
+        interpretBMI(bmi);
+
+        scanner.close();
+    }
+
+    public static double calculateBMI(double weight, double height) {
+        return weight / (height * height);
+    }
+
+    public static void interpretBMI(double bmi) {
+        if (bmi < 18.5) {
+            System.out.println("You are underweight.");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("You are of normal weight.");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.println("You are overweight.");
+        } else {
+            System.out.println("You are obese.");
+        }
+    }
 
 	//Enter the code here....
 }
